3e28ba586db3cbad69b906159b65412854d24053
Ruben <rgalindo@enssat.fr>
2020-10-27 17:03:18 +0100

    deuxième commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import demo.Money;
+
+public class Main {
+    public static void main(String[] args) {
+        Money test= new Money(42,"EUR");
+        System.out.println(test.toString());
+    }
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,4 +1,18 @@
 package demo;
 
 public class Money {
+    int amout;
+    String devise;
+
+    public  Money(int n, String s){
+        amout=n;
+        devise=s;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amout=" + amout + devise + '\'' +
+                '}';
+    }
 }
