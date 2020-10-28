4355507fb3e20c703e8ec310b2f7a827c671e17d
Ruben <rgalindo@enssat.fr>
2020-10-28 11:19:38 +0100

    troisième commit

--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,18 +1,38 @@
 package demo;
 
+import java.util.Objects;
+
+
 public class Money {
-    int amout;
+    int amount;
     String devise;
 
     public  Money(int n, String s){
-        amout=n;
+        amount=n;
         devise=s;
+    }
+    public Money add(Money that) {
+        return new Money(this.amount+that.amount,this.devise);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount &&
+                Objects.equals(devise, money.devise);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, devise);
     }
 
     @Override
     public String toString() {
         return "Money{" +
-                "amout=" + amout + devise + '\'' +
+                "amount=" + amount + devise + '\'' +
                 '}';
     }
 }
